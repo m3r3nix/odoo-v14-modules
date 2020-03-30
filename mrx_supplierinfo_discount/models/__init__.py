a8f626ff928267decdb65316b3269bbdd9788103
sfu420 <freedom.of.p2p@gmail.com>
2020-03-30 15:25:49 +0000

    Reorder import in alphabetical order

--- mrx_supplierinfo_discount/models/__init__.py
+++ mrx_supplierinfo_discount/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from . import account_invoice
 from . import product_category_vendordiscount
 from . import product_supplierinfo
 from . import purchase
 from . import stock_rule
-from . import account_invoice
