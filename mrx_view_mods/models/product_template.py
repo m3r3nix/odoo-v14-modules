86a8d2c36f3ae383ed9743b46e0ae205cdfa5f4d
sfu420 <freedom.of.p2p@gmail.com>
2020-04-28 18:56:07 +0000

    Fix missing attributes

--- mrx_view_mods/models/product_template.py
+++ mrx_view_mods/models/product_template.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    intrastat_origin_country_code = fields.Char(related='intrastat_origin_country_id.code', string='Country Code', readonly=True, size=2)
+    intrastat_origin_country_code = fields.Char(related='intrastat_origin_country_id.code', string='Country Code', readonly=True, store=True, copy=True, size=2)
