d7012d7cec8f0a169a08fba21565ec92d889297f
sfu420 <freedom.of.p2p@gmail.com>
2020-04-29 19:27:26 +0000

    Build SO-PO relation table and fields

--- mrx_so_po_relation/models/models.py
+++ mrx_so_po_relation/models/models.py
@@ -1,18 +1,24 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class mrx_so_po_relation(models.Model):
-#     _name = 'mrx_so_po_relation.mrx_so_po_relation'
-#     _description = 'mrx_so_po_relation.mrx_so_po_relation'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    mrx_po_ids = fields.Many2many('purchase.order', 'purchase_order_sale_order_rel', 'sale_order_id', 'purchase_order_id', readonly=True, store=True)
+    
+#    mrx_po_bill_ids = fields.Many2many()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    
+    mrx_so_ids = fields.Many2many('sale.order', 'purchase_order_sale_order_rel', 'purchase_order_id', 'sale_order_id', compute='_compute_so_po_ids', readonly=True, store=True)
+    
+    # Search for sales orders based on the origin field in PO
+    @api.depends('origin')
+    def _compute_so_po_ids(self):
+        for line in self:
+            if line.origin:
+                line.mrx_so_ids = self.env['sale.order'].search([('name','=',line.origin.split(', '))])
