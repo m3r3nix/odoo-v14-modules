0914e2289571ce63b1b659946f3c22aaaad691df
sfu420 <freedom.of.p2p@gmail.com>
2020-04-29 14:04:47 +0000

    Fix unintended record creation of new manufacturer

--- mrx_product_manufacturer/models/models.py
+++ mrx_product_manufacturer/models/models.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 
+
 # Creates own table in database to store name of Manufacturers
 class ProductManufacturer(models.Model):
     _name = 'mrx.product.manufacturer'
@@ -9,10 +10,11 @@ class ProductManufacturer(models.Model):
 
     name = fields.Char(required=True, store=True, copy=True)
 
-    _sql_constraints = [('name_uniq', 'unique(name)', 'Manufacturer already exists!')]
+    _sql_constraints = [('unique_name', 'unique(name)', 'Manufacturer already exists!')]
+
 
 # Creates Manufacturer field in product table
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    
+
     mrx_product_manufacturer = fields.Many2one('mrx.product.manufacturer', string='Manufacturer', index=True)
