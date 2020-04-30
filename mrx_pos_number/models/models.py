21c38d998a7784f8266ba9260b86ad2d9cc90f6c
sfu420 <freedom.of.p2p@gmail.com>
2020-04-30 19:09:27 +0000

    Fix typo

--- mrx_pos_number/models/models.py
+++ mrx_pos_number/models/models.py
@@ -3,14 +3,14 @@
 from odoo import models, fields, api
 
 
-class SaleOrder(models.Model):
-    _inherited = 'sale.order'
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
-    mrx_pos_number = fields.Integer(compute="_compute_pos_number", string="Pos.", readonly=True, store=True)
+    mrx_pos_number = fields.Integer(compute='_compute_pos_number', string='Pos.', readonly=True, store=True)
     
-    @api.depends("sequence", "order_id")
+    @api.depends('sequence', 'order_id')
     def _compute_pos_number(self):
-        for order in self.mapped("order_id"):
+        for order in self.mapped('order_id'):
             mrx_pos_number = 1
             for line in order.order_line:
                 line.mrx_pos_number = mrx_pos_number
