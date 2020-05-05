c1500538833a82bdc652e6ae830a45b7dde04710
sfu420 <freedom.of.p2p@gmail.com>
2020-05-05 18:27:23 +0000

    Calculate SO Profit seems to be ready :)

--- mrx_so_profit/models/models.py
+++ mrx_so_profit/models/models.py
@@ -1,54 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     mrx_so_profit = fields.Float(compute='_compute_mrx_so_profit', string="Profit", readonly=True, store=True)
 
-    @api.depends('amount_untaxed', 'invoice_ids', 'invoice_ids.state', 'mrx_po_ids', 'mrx_po_ids.state', 'mrx_po_ids.invoice_count', 'mrx_po_bill_ids', 'mrx_po_bill_ids.state')
+    @api.depends('state', 'amount_untaxed', 'invoice_ids.state', 'mrx_po_ids.amount_untaxed', 'mrx_po_ids.state', 'mrx_po_ids.invoice_count', 'mrx_po_bill_ids.state')
     def _compute_mrx_so_profit(self):
         for record in self:
-            sum_so_bills = sum_po_bills = 0.0
-            if record.invoice_status == "invoiced" and record.invoice_ids:
-                for bill in record.invoice_ids:
-                    if bill.state == "posted":
+            if record.state != 'cancel':
+                sum_so_bills = sum_po_bills = 0.0
+                posted_invoices = record.invoice_ids.filtered(lambda move: move.state == 'posted')
+                _logger.warning("Sales Invoices are posted: %s:", posted_invoices)
+                if posted_invoices:
+                    for bill in posted_invoices:
                         sum_so_bills += bill.amount_untaxed
-#                    else:
-#                        break
-            else:
-                sum_so_bills = record.amount_untaxed
-            if record.mrx_po_ids:
-                for po in record.mrx_po_ids:
-                    if po.state == "purchase" or po.state == "done":
-                        if po.invoice_status == "invoiced" and po.invoice_ids:
-                            for bill in po.invoice_ids:
-                                if bill.state == "posted":
+                else:
+                    sum_so_bills = record.amount_untaxed
+                _logger.warning("SUM SO Invoices: %s:", sum_so_bills)
+                if record.mrx_po_ids:
+                    for po in record.mrx_po_ids:
+                        if po.state == "purchase" or po.state == "done":
+                            posted_invoices = po.invoice_ids.filtered(lambda move: move.state == 'posted')
+                            _logger.warning("Vendor Bills are posted: %s:", posted_invoices)
+                            if posted_invoices:
+                                for bill in posted_invoices:
                                     sum_po_bills += bill.amount_untaxed
-                        else:
-                            sum_po_bills += po.amount_untaxed
-            record.mrx_so_profit = sum_so_bills - sum_po_bills
-
-# Hozzá kell adni a po_bill_ids-t az SO-hoz, hogy a po_bill-ek state-jére is tudjam triggerelni a számolást
-# po_bill_ids talán új tab-ra, ha nem mutat jól...
-#
-#
-#for record in self:
-#  sum_so_bills = sum_po_bills = 0.0
-#  if record.invoice_ids:
-#    for bill in record.invoice_ids:
-#      if bill.state == "open" or bill.state == "paid":
-#        sum_so_bills += bill.amount_untaxed
-#  else:
-#    sum_so_bills = record.amount_untaxed
-#  if record.x_studio_field_po_ids:
-#    for po in record.x_studio_field_po_ids:
-#      if po.state == "purchase" or po.state == "done":
-#        if po.invoice_status == "invoiced" and po.invoice_ids:
-#          for bill in po.invoice_ids:
-#            sum_po_bills += bill.amount_untaxed
-#        else:
-#          sum_po_bills += po.amount_untaxed
-#  record['x_studio_profit'] = sum_so_bills - sum_po_bills
+                            else:
+                                sum_po_bills += po.amount_untaxed
+                _logger.warning("SUM PO Invoices: %s:", sum_po_bills)
+                record.mrx_so_profit = sum_so_bills - sum_po_bills
+            else:
+                record.mrx_so_profit = 0.0
+                _logger.warning("SO Canceled: %s:", record.mrx_so_profit)
