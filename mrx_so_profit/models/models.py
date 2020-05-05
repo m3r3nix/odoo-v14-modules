e0113ad7419f569be7d843d0780dec9b5b297939
sfu420 <freedom.of.p2p@gmail.com>
2020-05-05 18:29:07 +0000

    Comment out logging lines

--- mrx_so_profit/models/models.py
+++ mrx_so_profit/models/models.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-import logging
-_logger = logging.getLogger(__name__)
+#import logging
+#_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -15,25 +15,25 @@ class SaleOrder(models.Model):
             if record.state != 'cancel':
                 sum_so_bills = sum_po_bills = 0.0
                 posted_invoices = record.invoice_ids.filtered(lambda move: move.state == 'posted')
-                _logger.warning("Sales Invoices are posted: %s:", posted_invoices)
+#                _logger.warning("Sales Invoices are posted: %s:", posted_invoices)
                 if posted_invoices:
                     for bill in posted_invoices:
                         sum_so_bills += bill.amount_untaxed
                 else:
                     sum_so_bills = record.amount_untaxed
-                _logger.warning("SUM SO Invoices: %s:", sum_so_bills)
+#                _logger.warning("SUM SO Invoices: %s:", sum_so_bills)
                 if record.mrx_po_ids:
                     for po in record.mrx_po_ids:
                         if po.state == "purchase" or po.state == "done":
                             posted_invoices = po.invoice_ids.filtered(lambda move: move.state == 'posted')
-                            _logger.warning("Vendor Bills are posted: %s:", posted_invoices)
+#                            _logger.warning("Vendor Bills are posted: %s:", posted_invoices)
                             if posted_invoices:
                                 for bill in posted_invoices:
                                     sum_po_bills += bill.amount_untaxed
                             else:
                                 sum_po_bills += po.amount_untaxed
-                _logger.warning("SUM PO Invoices: %s:", sum_po_bills)
+#                _logger.warning("SUM PO Invoices: %s:", sum_po_bills)
                 record.mrx_so_profit = sum_so_bills - sum_po_bills
             else:
                 record.mrx_so_profit = 0.0
-                _logger.warning("SO Canceled: %s:", record.mrx_so_profit)
+#                _logger.warning("SO Canceled: %s:", record.mrx_so_profit)
