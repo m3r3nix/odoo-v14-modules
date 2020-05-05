d45357114c1e625a8e6725072d46236a04b4bc3f
sfu420 <freedom.of.p2p@gmail.com>
2020-05-05 12:26:15 +0000

    Some improvements and cosmetics

--- mrx_so_po_relation/models/models.py
+++ mrx_so_po_relation/models/models.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    mrx_po_ids = fields.Many2many('purchase.order', 'purchase_order_sale_order_rel', 'sale_order_id', 'purchase_order_id', readonly=True, store=True)
-    mrx_po_bill_ids = fields.Many2many('account.move', 'sale_order_purchase_order_bill_rel', 'sale_order_id', 'account_move_id', compute='_compute_po_bill_ids', readonly=True, store=True)
+    mrx_po_ids = fields.Many2many('purchase.order', 'purchase_order_sale_order_rel', 'sale_order_id', 'purchase_order_id', string="Purchase Orders", readonly=True, store=True)
+    mrx_po_bill_ids = fields.Many2many('account.move', 'sale_order_purchase_order_bill_rel', 'sale_order_id', 'account_move_id', compute='_compute_po_bill_ids', string="Vendor Bills", readonly=True, store=True)
 
     # Search for po bills based on the origin field on invoice
     @api.depends('mrx_po_ids.invoice_count')
@@ -24,8 +24,8 @@ class SaleOrder(models.Model):
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    mrx_so_ids = fields.Many2many('sale.order', 'purchase_order_sale_order_rel', 'purchase_order_id', 'sale_order_id', compute='_compute_so_po_ids', readonly=True, store=True)
-    mrx_so_bill_ids = fields.Many2many('account.move', 'purchase_order_sale_order_bill_rel', 'purchase_order_id', 'account_move_id', compute='_compute_so_bill_ids', readonly=True, store=True)
+    mrx_so_ids = fields.Many2many('sale.order', 'purchase_order_sale_order_rel', 'purchase_order_id', 'sale_order_id', compute='_compute_so_po_ids', string="Sales Orders", readonly=True, store=True)
+    mrx_so_bill_ids = fields.Many2many('account.move', 'purchase_order_sale_order_bill_rel', 'purchase_order_id', 'account_move_id', compute='_compute_so_bill_ids', string="Sales Bills", readonly=True, store=True)
 
     # Search for sales orders based on the origin field in PO
     @api.depends('origin')
