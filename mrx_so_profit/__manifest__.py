1f807041e86e09822fd4f891250dd2335135d6a9
sfu420 <freedom.of.p2p@gmail.com>
2020-05-05 18:36:56 +0000

    Bump version number

--- mrx_so_profit/__manifest__.py
+++ mrx_so_profit/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '13.0.0.1',
+    'version': '13.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
