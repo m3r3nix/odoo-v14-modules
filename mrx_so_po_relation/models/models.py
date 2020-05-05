51e0fbcdbe094683a5c4497520d7476403abe01e
sfu420 <freedom.of.p2p@gmail.com>
2020-05-05 11:40:37 +0000

    Create "mrx_so_bill_ids" on Purchase Orders

--- mrx_so_po_relation/models/models.py
+++ mrx_so_po_relation/models/models.py
@@ -9,6 +9,7 @@ class SaleOrder(models.Model):
     mrx_po_ids = fields.Many2many('purchase.order', 'purchase_order_sale_order_rel', 'sale_order_id', 'purchase_order_id', readonly=True, store=True)
     mrx_po_bill_ids = fields.Many2many('account.move', 'sale_order_purchase_order_bill_rel', 'sale_order_id', 'account_move_id', compute='_compute_po_bill_ids', readonly=True, store=True)
 
+    # Search for po bills based on the origin field on invoice
     @api.depends('mrx_po_ids.invoice_count')
     def _compute_po_bill_ids(self):
         for record in self:
@@ -24,6 +25,7 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     mrx_so_ids = fields.Many2many('sale.order', 'purchase_order_sale_order_rel', 'purchase_order_id', 'sale_order_id', compute='_compute_so_po_ids', readonly=True, store=True)
+    mrx_so_bill_ids = fields.Many2many('account.move', 'purchase_order_sale_order_bill_rel', 'purchase_order_id', 'account_move_id', compute='_compute_so_bill_ids', readonly=True, store=True)
 
     # Search for sales orders based on the origin field in PO
     @api.depends('origin')
@@ -31,3 +33,14 @@ class PurchaseOrder(models.Model):
         for line in self:
             if line.origin:
                 line.mrx_so_ids = self.env['sale.order'].search([('name','=',line.origin.split(', '))])
+
+    # Search for so bills based on the origin field on invoice
+    @api.depends('mrx_so_ids.invoice_count')
+    def _compute_so_bill_ids(self):
+        for record in self:
+            xyz = []
+            if record.mrx_so_ids:
+                for line in record.mrx_so_ids:
+                    xyz.append(line.name)
+                result = self.env['account.move'].search([('invoice_origin','=',xyz)])
+                record.mrx_so_bill_ids = result
