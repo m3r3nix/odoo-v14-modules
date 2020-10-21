fd844954a85870c4980fc82e0db8ae2fafa26354
sfu420 <freedom.of.p2p>
2020-10-21 20:42:55 +0200

    manufacturer: Change module description

--- mrx_product_manufacturer/__manifest__.py
+++ mrx_product_manufacturer/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mrx_product_manufacturer",
+    'name': "Product manufacturer",
 
-    'summary': "Add Manufacturer of product option",
+    'summary': "Add manufacturer filed to product",
 
-    'description': "Add Manufacturer of product option",
+    'description': "Add manufacturer field to product",
 
     'author': "MERENIX Elektrohandel e.K.",
     'website': "",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Product',
     'version': '14.0.1.0',
