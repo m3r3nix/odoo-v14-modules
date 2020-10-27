75491bc176a3e14453f3044bed0b53ad15fb3e37
sfu420 <freedom.of.p2p>
2020-10-27 17:32:26 +0100

    so_profit: Update module description file

--- mrx_so_profit/__manifest__.py
+++ mrx_so_profit/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mrx_so_profit",
+    'name': "SO Profit",
 
     'summary': "Calculate profit for a sales order like sum(so.bills)-sum(po.bills)",
 
@@ -10,16 +10,17 @@
     'website': "",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '13.0.1.0',
+    'version': '14.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
                 'sale_management',
                 'purchase',
                 'account',
+                'mrx_so_po_relation',
     ],
 
     # always loaded
