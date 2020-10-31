578d3b6c3210c9f9171c08826e7cb2ff9ef70ea4
sfu420 <freedom.of.p2p>
2020-10-31 20:45:48 +0100

    vendorDiscount: Rename model to mrx.vendordiscount

--- mrx_supplier_discount/models/__init__.py
+++ mrx_supplier_discount/models/__init__.py
@@ -2,9 +2,9 @@
 
 # I do not adapt "mrx_pricing_unit" in v13 to the accounting app, instead hand over already calculated value to "price_unit"
 # from . import account_invoice
-from . import product__product
-from . import product_category_vendordiscount
-from . import product_supplierinfo
-from . import purchase__purchase
-from . import purchase_stock__stock_rule
-from . import sale__sale
+# from . import product__product
+# from . import product_supplierinfo
+# from . import purchase__purchase
+# from . import purchase_stock__stock_rule
+# from . import sale__sale
+from . import vendordiscount
