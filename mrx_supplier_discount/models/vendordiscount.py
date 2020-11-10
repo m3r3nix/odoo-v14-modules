8f11991711e4d5cdf68e4783e7a6bb4432ade440
sfu420 <freedom.of.p2p@gmail.com>
2020-11-10 19:24:12 +0100

    vendorDiscount: remane model and field

--- mrx_supplier_discount/models/vendordiscount.py
+++ mrx_supplier_discount/models/vendordiscount.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models  # alphabetically ordered
+from odoo import fields, models  # alphabetically ordered
 
 # Create own table in database to store vendor discounts by product category
 class VendorDiscount(models.Model):
-    _name = "mrx.vendordiscount"
-    _description = "Price groups & Discounts by Vendor"
+    _name = 'mrx.product.vendordiscount'
+    _description = 'Price groups & Discounts by Vendor'
     
     partner_id = fields.Many2one('res.partner', string='Vendor', domain=[('supplier_rank', '>', 0)], ondelete='cascade', required=True)
-    manufacturer = fields.Many2one('mrx.product.manufacturer', string='Manufacturer', required=True)
+    manufacturer_id = fields.Many2one('mrx.product.manufacturer', string='Manufacturer', required=True)
     name = fields.Char(string='Price Group', index=True, required=True, help="Name of the product price group or discount group")
     discount = fields.Float(string='Discount %', default=0.0, digits='Discount', required=True, help="Discount percent given by this vendor")
     
-    _sql_constraints = [('discount_uniq', 'unique(partner_id, manufacturer, name)', 'This type of discount already exists!')]
+    _sql_constraints = [('discount_uniq', 'unique(partner_id, manufacturer_id, name)', 'This type of discount already exists!')]
