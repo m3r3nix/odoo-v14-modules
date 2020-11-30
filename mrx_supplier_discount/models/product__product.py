80600a097ddd4811b14874d9ec2472e41213ce0b
sfu420 <freedom.of.p2p@gmail.com>
2020-11-30 18:26:41 +0100

    vendorDiscount: Remove unused dependancy

--- mrx_supplier_discount/models/product__product.py
+++ mrx_supplier_discount/models/product__product.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools, _
+from odoo import api, fields, models
 from odoo.tools import float_compare
 
 class ProductTemplate(models.Model):
