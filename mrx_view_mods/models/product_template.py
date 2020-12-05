1fd9ceeb0a529db9dcb35cc7663d4c6cb895646d
sfu420 <freedom.of.p2p@gmail.com>
2020-12-05 16:13:45 +0100

    viewMods: Code cleanup

--- mrx_view_mods/models/product_template.py
+++ mrx_view_mods/models/product_template.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
-# Creates Country Code field in product template
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    # Add extra filed to product template model
     intrastat_origin_country_code = fields.Char(related='intrastat_origin_country_id.code', string='Country Code', readonly=True, store=True, copy=True, size=2)
