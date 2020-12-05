814c29d606ad65c3b19b987a35735b7e072990a9
sfu420 <freedom.of.p2p@gmail.com>
2020-12-05 16:09:18 +0100

    viewMods: Update module manifest to v14

--- mrx_view_mods/__manifest__.py
+++ mrx_view_mods/__manifest__.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mrx_view_mods",
+    'name': "View Mods",
 
-    'summary': "Modify default views for MERENIX",
+    'summary': "Modify default views for MERENIX use cases",
 
-    'description': "Modify default views for MERENIX",
+    'description': "Modify default views for MERENIX use cases",
 
     'author': "MERENIX Elektrohandel e.K.",
     'website': "",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '13.0.1.0',
+    'version': '14.0.1.0',
 
-    # any module necessary for this one to work correctly
     'depends': [
         'base',
         'product',
@@ -25,9 +24,7 @@
         'account_intrastat',
     ],
 
-    # always loaded
     'data': [
-#        'security/ir.model.access.csv',
         'views/product_template.xml',
         'views/sale_order.xml',
         'views/product_category.xml',
