ee8662bc053e0c0f496e41c29175b7f7f21b2c76
sfu420 <freedom.of.p2p@gmail.com>
2020-12-09 20:15:00 +0100

    posNumber: Adapt manifest to v14

--- mrx_pos_number/__manifest__.py
+++ mrx_pos_number/__manifest__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mrx_pos_number",
+    'name': "Position Number",
 
-    'summary': "Add Pos. number to SO, PO and DN",
+    'summary': "Add Pos. number field to SO, PO and DN",
 
-    'description': "Add Pos. number to SO, PO and DN",
+    'description': "Add Pos. number field to SO, PO and DN",
 
     'author': "MERENIX Elektrohandel e.K.",
     'website': "",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '13.0.0.1',
+    'version': '14.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': [
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-#        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
     ],
 }
