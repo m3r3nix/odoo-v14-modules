4a9838cbf83f01f0872c4af6fb06c4c2df79d0e1
sfu420 <freedom.of.p2p@gmail.com>
2020-12-10 22:10:20 +0100

    posNumber: Add Pos number to PO

---
+++ mrx_pos_number/models/purchase_purchase.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    mrx_pos_number = fields.Integer(compute='_compute_pos_number', string='Pos.')
+
+    def _compute_pos_number(self):
+        counter = 1
+        for line in self.order_id.order_line:
+            line.mrx_pos_number = counter
+            counter += 1
