1ea5098ab94a4031193039af98f38cc73dfdccc2
sfu420 <freedom.of.p2p@gmail.com>
2020-12-11 02:05:47 +0100

    posNumber: Add Pos number to stock delivery

--- mrx_pos_number/models/stock_stock_move.py
+++ mrx_pos_number/models/stock_stock_move.py
@@ -6,4 +6,6 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    # Add some extra fields to the model
+    mrx_pos_number_delivery = fields.Integer(related='sale_line_id.mrx_pos_number', string='Pos.')
     mrx_pos_number_receipt = fields.Integer(related='purchase_line_id.mrx_pos_number', string='Pos.')
