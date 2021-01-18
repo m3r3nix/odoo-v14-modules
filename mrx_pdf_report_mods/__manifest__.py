833254a6d01b87a29068e30a50d02fe4bcf684d4
sfu420 <freedom.of.p2p@gmail.com>
2021-01-18 17:32:30 +0100

    pdfMods: Initial commit of this new module

---
+++ mrx_pdf_report_mods/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PDF report custom mods",
+
+    'summary': """
+        This module contains my custom modifications for PDF reports""",
+
+    'description': """
+        This module contains my custom modifications for PDF reports such as invoice, delivery note, etc...
+    """,
+
+    'author': "MERENIX Elektrohandel e.K.",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '14.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'account',
+        'stock',
+    ],
+
+    # always loaded
+    'data': [
+        # 'views/views.xml',
+        'views/templates.xml',
+    ],
+}
