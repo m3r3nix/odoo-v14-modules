c43dd6ace7e14e87bc76f693698b8e6059ba1c26
m3r3nix <m3r3nix@protonmail.com>
2021-04-21 13:13:12 +0200

    deliveryNoteInvoice: Introduce delivery note numer on invoice line module

---
+++ mrx_delivery_note_on_invoice/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Delivery Note on Invoice",
+
+    'summary': """
+        Display delivery note number on invoice line
+    """,
+
+    'description': """
+        Display delivery note number on invoice line
+    """,
+
+    'author': "MERENIX Elektrohandel e.K.",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Invoicing',
+    'version': '14.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'account',
+        'sale',
+        'stock',
+    ],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+}
---
+++ mrx_delivery_note_on_invoice/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    mrx_delivery_note_reference = fields.Char(string='Delivery Note', store=True)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _prepare_invoice_line(self, **optional_values):
+        res = super()._prepare_invoice_line(**optional_values)
+        res['mrx_delivery_note_reference'] = self.move_ids.filtered(lambda x: x.state == 'done' and x.picking_code == 'outgoing').sorted('reference', reverse=True)[0].reference
+        return res
