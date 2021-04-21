4cdbfa5b912d72ae20428d610f3cb922b9d76528
m3r3nix <m3r3nix@protonmail.com>
2021-04-21 13:15:04 +0200

    pdfMods: Change module category

--- mrx_pdf_report_mods/__manifest__.py
+++ mrx_pdf_report_mods/__manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Customizations',
     'version': '14.0.1.0',
 
     # any module necessary for this one to work correctly
@@ -24,6 +24,7 @@
         'account',
         'account_intrastat',
         'stock',
+        'mrx_delivery_note_on_invoice',
     ],
 
     # always loaded
