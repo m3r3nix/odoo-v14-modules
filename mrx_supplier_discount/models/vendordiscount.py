13efc9f244117d29a8159577580f124754db01a3
m3r3nix <m3r3nix@protonmail.com>
2021-06-04 19:12:44 +0200

    vendorDiscount: Fix for database related mistakes

--- mrx_supplier_discount/models/vendordiscount.py
+++ mrx_supplier_discount/models/vendordiscount.py
@@ -3,14 +3,20 @@
 from odoo import fields, models
 
 
-# Create own table in database to store list of price groups
+class ProductManufacturer(models.Model):
+    _inherit = 'mrx.product.manufacturer'
+
+    pricegroup_ids = fields.One2many('mrx.manufacturer.pricegroup', 'manufacturer_id', string="Price Groups")
+
+# Create own table in database to store price groups
 class PriceGroup(models.Model):
     _name = 'mrx.manufacturer.pricegroup'
-    _description = 'Price groups by Manufacturer'
+    _description = 'Price Groups by Manufacturer'
     _order = 'manufacturer_id, name'
 
     manufacturer_id = fields.Many2one('mrx.product.manufacturer', string='Manufacturer', required=True)
     name = fields.Char(string='Price Group', index=True, required=True, help="Name of the product price group or discount group")
+    partner_ids = fields.One2many('mrx.product.vendordiscount', 'name', string="Vendors")
 
     _sql_constraints = [('pricegroup_uniq', 'UNIQUE(manufacturer_id, name)', 'This type of price group already exists!')]
 
@@ -22,10 +28,10 @@ class VendorDiscount(models.Model):
     _order = 'partner_id, manufacturer_id, name'
     
     partner_id = fields.Many2one('res.partner', string='Vendor', ondelete='cascade', required=True)
-    manufacturer_id = fields.Many2one('mrx.product.manufacturer', string='Manufacturer', required=True)
+    manufacturer_id = fields.Many2one(related='name.manufacturer_id', readonly=True)
     name = fields.Many2one('mrx.manufacturer.pricegroup', string='Price Group', required=True)
     discount = fields.Float(string='Discount %', default=0.0, digits='Discount', required=True, help="Discount percent given by this vendor")
 
     _sql_constraints = [
-        ('discount_uniq', 'UNIQUE(partner_id, manufacturer_id, name)', 'This type of discount already exists!'),
+        ('discount_uniq', 'UNIQUE(partner_id, name)', 'This type of discount already exists!'),
     ]
