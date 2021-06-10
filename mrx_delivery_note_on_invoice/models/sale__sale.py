d961484fceb6e653ec0a821463173fb771998d28
m3r3nix <m3r3nix@protonmail.com>
2021-06-10 21:49:18 +0200

    deliveryToInvoice: Fix delivery cost missing reference number issue

--- mrx_delivery_note_on_invoice/models/sale__sale.py
+++ mrx_delivery_note_on_invoice/models/sale__sale.py
@@ -8,5 +8,7 @@ class SaleOrderLine(models.Model):
 
     def _prepare_invoice_line(self, **optional_values):
         res = super()._prepare_invoice_line(**optional_values)
-        res['mrx_delivery_note_reference'] = self.move_ids.filtered(lambda x: x.state == 'done' and x.picking_code == 'outgoing').sorted('reference', reverse=True)[0].reference
+        line = self.move_ids.filtered(lambda x: x.state == 'done' and x.picking_code == 'outgoing').sorted('reference', reverse=True)
+        if line:
+            res['mrx_delivery_note_reference'] = line[0].reference
         return res
