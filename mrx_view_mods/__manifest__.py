ec82b99c527f6f4f7ff2e3514607dd3429aa707a
m3r3nix <m3r3nix@protonmail.com>
2021-06-10 19:48:16 +0200

    viewMods: Hide HS Code field on product

--- mrx_view_mods/__manifest__.py
+++ mrx_view_mods/__manifest__.py
@@ -13,10 +13,11 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '14.0.1.0',
+    'version': '14.0.1.1',
 
     'depends': [
         'base',
+        'delivery',
         'product',
         'purchase',
         'purchase_stock',
